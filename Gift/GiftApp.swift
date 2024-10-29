3d6d9f752481404dfbab63b525abd52e4a4321a3
Eason Lin <lin00212@algonquinlive.com>
2024-10-29 19:30:30 -0400

    Initial Commit

---
+++ Gift/GiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  GiftApp.swift
+//  Gift
+//
+//  Created by Eason Lin on 29/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
